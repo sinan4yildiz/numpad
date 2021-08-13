ed1c0864869d4b9382db79e12a3896c23fe2d05b
Sinan <snn@sinanyildiz.me>
2021-08-13 19:42:17 +0300

    First distribution

--- src/index.js
+++ src/index.js
@@ -2,63 +2,107 @@
     "use strict";
 
     /*
-    * @param {string} el The element to which the keypad is attached.
+    * @param {string} element
+    * The element to which the keypad is attached.
     *
     * */
     class numericKeypad {
-        constructor(el) {
-            this.el = el;
-
-            this.nums = [
-                {
-                    value: 1,
-                    color: "#6F98A8",
-                },
-                {
-                    value: 2,
-                    color: "#2B8EAD",
-                },
-                {
-                    value: 3,
-                    color: "#2F454E",
-                },
-                {
-                    value: 4,
-                    color: "#2B8EAD",
-                },
-                {
-                    value: 5,
-                    color: "#2F454E",
-                },
-                {
-                    value: 6,
-                    color: "#BFBFBF",
-                },
-                {
-                    value: 7,
-                    color: "#BFBFBF",
-                },
-                {
-                    value: 8,
-                    color: "#6F98A8",
-                },
-                {
-                    value: 9,
-                    color: "#2F454E",
-                },
-            ]
+        constructor(element) {
+            this.element = element;
         }
 
+        /*
+        * Renders the keypad into specified element
+        *
+        * */
+        render(shuffle = false) {
 
-        render() {
-            document.querySelector(this.el).innerHTML = '<ol>123</ol>';
+            /*
+            * Create parent element
+            *
+            * */
+            const ol = document.createElement('ol');
+
+            for (var i = 1; i <= 9; i++) {
+
+                /*
+                * Create child elements
+                *
+                * */
+                const li = document.createElement('li'),
+                    button = document.createElement('button');
+
+                /*
+                * Button text and attributes
+                *
+                * */
+                button.setAttribute('type', 'button');
+                button.setAttribute('data-value', i);
+                button.innerText = i;
+
+                // Append the number button
+                li.append(button);
+
+                // Append the list item
+                ol.appendChild(li);
+            }
+
+
+            /*
+            * Shuffle the numbers
+            *
+            * */
+            if(shuffle) {
+                for (var c = ol.children.length; c >= 0; c--) {
+                    ol.appendChild(ol.children[Math.random() * c | 0]);
+                }
+            }
+
+
+            /*
+            * Put the markup
+            *
+            * */
+            const parent = document.querySelector(this.element);
+
+            if(parent.firstChild) {
+                parent.firstChild.remove();
+            }
+
+            parent.appendChild(ol);
         }
     }
 
-    // Create an instance
-    const numPad = new numericKeypad('article');
+    /*
+    * Create an instance
+    *
+    * */
+    const numPad = new numericKeypad("article");
 
-    // Initial rendering
+
+    /*
+    * Initial rendering
+    *
+    * */
     numPad.render();
+
+
+    /*
+    * Shuffle numbers
+    *
+    * */
+    document.querySelector('#shuffle').addEventListener('click', () => {
+        numPad.render(true);
+    });
+
+
+    /*
+    * Sort numbers
+    *
+    * */
+    document.querySelector('#sort').addEventListener('click', () => {
+        numPad.render();
+    });
+
 
 })();
